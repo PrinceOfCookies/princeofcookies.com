4026728d0a91946b63270ddd308bfad127ad8ebd
PrinceOfCookies <prince@strw.club>
2025-09-29 00:28:23 -0500

    forgot to commit these changes

--- src/server.js
+++ src/server.js
@@ -1,2 +1,16 @@
-import { initWebSocket } from './build/lib/server/websocket.js';
+import express from "express";
+import http from "http";
+import { handler } from "./build/handler.js";
+import { initWebSocket } from "./build/lib/server/websocket.js";
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(handler);
+
 initWebSocket(server);
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`✅ Server listening on http://localhost:${port}`);
+});
