1a01968397877968aca905e4cebfb18f7b484eb0
PrinceOfCookies <iuploadgmod@gmail.com>
2026-08-28 20:42:26 -0700

    Create agnesa-beqiri/+page.svelte

---
+++ src/routes/scprp/agnesa-beqiri/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+  import CharacterDossier from "$lib/components/dossier.svelte";
+  import { getScpCharacterJsonPath } from "$lib/scp-routing";
+</script>
+
+<CharacterDossier jsonPath={getScpCharacterJsonPath("agnesa-beqiri")} />
